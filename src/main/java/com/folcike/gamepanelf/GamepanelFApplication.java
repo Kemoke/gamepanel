be12dc4ed87a6e688084c472089b770c4fe297a5
Kemal Hrelja <kemal@digi.me>
2018-03-09 11:21:26 +0100

    Project setup

---
+++ src/main/java/com/folcike/gamepanelf/GamepanelFApplication.java
@@ -0,0 +1,12 @@
+package com.folcike.gamepanelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepanelFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepanelFApplication.class, args);
+	}
+}
---
+++ src/test/java/com/folcike/gamepanelf/GamepanelFApplicationTests.java
@@ -0,0 +1,16 @@
+package com.folcike.gamepanelf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamepanelFApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
