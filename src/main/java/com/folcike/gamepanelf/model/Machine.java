8d3585c2c51ec538bc1d2043485342398bbb8d61
Ali Hashemi <salehisaleh50@gmail.com>
2018-03-19 21:25:23 +0100

    Machine Model

---
+++ src/main/java/com/folcike/gamepanelf/model/Machine.java
@@ -0,0 +1,23 @@
+package com.folcike.gamepanelf.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Machine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+
+    private String address;
+
+    private String password;
+}
