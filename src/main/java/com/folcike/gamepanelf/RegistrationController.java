eb736c1c3d00ec41ecc349437c90f2a3f20728fd
selvindenis97 <stambolicselvindenis@gmail.com>
2018-04-08 11:30:55 +0200

    registration fixes

--- src/main/java/com/folcike/gamepanelf/RegistrationController.java
+++ src/main/java/com/folcike/gamepanelf/RegistrationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/Registration")
+@RequestMapping("/registration")
 
 public class RegistrationController {
 
