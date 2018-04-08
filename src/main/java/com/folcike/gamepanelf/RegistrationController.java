7083ce2397dd87744bc60b923ad2af1972ca57ab
selvindenis97 <stambolicselvindenis@gmail.com>
2018-04-08 11:00:27 +0200

    Connection between model and form

---
+++ src/main/java/com/folcike/gamepanelf/RegistrationController.java
@@ -0,0 +1,16 @@
+package com.folcike.gamepanelf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/Registration")
+
+public class RegistrationController {
+
+    @RequestMapping("/")
+    public String registration(Model model, RedirectAttributes redirect){ return "registration";}
+
+}
