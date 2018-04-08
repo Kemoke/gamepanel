7ff4c66a57087f786b47f26870197940501f8994
Kemoke <mail@kemoke.net>
2018-04-08 11:13:34 +0200

    Login exclusions

--- src/main/java/com/folcike/gamepanelf/SecurityConfig.java
+++ src/main/java/com/folcike/gamepanelf/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/login*","/home","/img/**", "/png/**").permitAll()
+                .antMatchers("/login*","/home","/img/**", "/png/**", "/css/**", "/registration*").permitAll()
                 .antMatchers("/**").authenticated()
                 .and()
                 .formLogin()//.loginPage("/login").failureUrl("/login?error")
